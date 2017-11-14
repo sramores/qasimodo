b6ba1aabafe8af50d4fdf983e8d1d0d0dfd2c115
Carlos Lucas <nosk14@gmail.com>
2017-11-14 21:14:30 +0100

    closes #2

--- src/handlers/response.py
+++ src/handlers/response.py
@@ -3,6 +3,6 @@ from . import event_controller as ec
 
 @ec.on("message")
 def response(event, client):
-    if 'qasimodo' in event['text']:
+    if 'text' in event and 'qasimodo' in event['text']:
         client.rtm_send_message(event['channel'], "k dise?")
 
