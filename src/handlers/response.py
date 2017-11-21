08e75d774b64df55da6f565d1ffe29ffbaa02e0f
ChemitaContigo <32568302+ChemitaContigo@users.noreply.github.com>
2017-11-21 13:08:19 +0100

    Añadimos nuevos mensajes
    Qasimodo dice mas cosas

--- src/handlers/response.py
+++ src/handlers/response.py
@@ -7,7 +7,16 @@ def response(event, client):
     if 'text' in event and 'qasimodo' in event['text']:
         msg = "k dise?"
         if event['user'] == constants.vsolis:
-            msg = "k dise bisente?"
-
+            msgBisente = ['Bisente pls, callate.', 'Sabes lo que eres. ¿No?', 'k dise Bisente']
+            msg =  msgBisente[randint(0, len(msgBisente)-1)]
+               if event['user'] == constants.semartin:
+                   msg = "Eres increible"
+                if event['user'] == constants.jrios:
+                    msg = "Loco, yo no soy de letras"
+                    if event['user'] ==constants.jariza:
+                      msgCordobe = ['k dise cordobe.', 'Toca bacat del dìa', 'Mas sospechoso que un gitano haciendo footing']
+                      msg =  msgCordobe[randint(0, len(msgCordobe)-1)]
+        
         client.rtm_send_message(event['channel'], msg)
+			
 
