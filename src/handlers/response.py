cec0b26aa391bdb8feaf544932a5ba13769c7d86
Carlos Lucas <clucas@hotelbeds.com>
2017-11-21 13:42:56 +0100

    indetation fixed

--- src/handlers/response.py
+++ src/handlers/response.py
@@ -6,7 +6,7 @@ import constants
 @ec.on("message")
 def response(event, client):
     if 'text' in event and 'qasimodo' in event['text'].lower():
-            msg = "k dise?"
+        msg = "k dise?"
         if event['user'] == constants.vsolis:
             msgBisente = ['Bisente pls, callate.', 'Sabes lo que eres. ¿No?', 'k dise Bisente']
             msg =  msgBisente[randint(0, len(msgBisente)-1)]
@@ -14,10 +14,10 @@ def response(event, client):
             msg = "Eres increible"
         if event['user'] == constants.jrios:
             msg = "Loco, yo no soy de letras"
-        if event['user'] ==constants.jariza:
+        if event['user'] == constants.jariza:
             msgCordobe = ['k dise cordobe.', 'Toca bacat del dìa', 'Mas sospechoso que un gitano haciendo footing']
             msg =  msgCordobe[randint(0, len(msgCordobe)-1)]
         
         client.rtm_send_message(event['channel'], msg)
-			
+
 
