7f8313070c1e752e1afaf471ef94dc63375eb65b
Carlos Lucas <clucas@hotelbeds.com>
2017-11-21 11:55:06 +0100

    improved morning sched and changed the cannel to be greet

--- src/sched/morning.py
+++ src/sched/morning.py
@@ -1,7 +1,18 @@
 from sched import scheduler
+from random import randint
+import datetime
 
+
+days_of_week = ['lunes', 'martes', 'miércoles', 'jueves',
+                'viernes', 'sábado', 'domingo']
+greetings = ['¡Buenos días!', '¡Que tengáis un fantástico día!',
+             'Good morning', 'Que paseis un buen {}', 'Buen {}', 'Buenos días por la mañana']
 
 @scheduler.scheduled_job('cron', day_of_week='mon-fri', hour=8)
 def morning(**kwargs):
+    msg = greetings[randint(0, len(greetings)-1)]
+    day = days_of_week[datetime.datetime.today().weekday()]
+    msg = msg.format(day)
+
     client = kwargs['client']
-    client.rtm_send_message("qasimodo_pruebas", 'Buenos dias!')
+    client.rtm_send_message("qa_group", msg)
