44fb3471abda12fa224fa3249c8b0801d3ec1e5f
sramores <sramores@gmail.com>
2018-04-12 17:30:16 +0200

    New Event - Feria of Cordoba

---
+++ src/sched/events.py
@@ -0,0 +1,20 @@
+from sched import scheduler
+from datetime import date
+
+@scheduler.scheduled_job('cron', day_of_week='fri', hour=8)
+def feria(**kwargs):
+    msn = ""
+    chn = "qa_group"
+
+    today = date.today()
+    feria = date(2018, 5, 18)
+
+    days = feria - today
+
+    if feria == today:
+        msn = ("Mañana empieza la feria de Cordoba. :tada: :dancer: :confetti_ball: :dancers:")
+    elif today < feria:
+        msn = ("Sólo faltan " + str(days) + " días para la feria de Cordoba. :tada: :dancer:")
+
+        client = kwargs['client']
+        client.rtm_send_message(chn, msn)
